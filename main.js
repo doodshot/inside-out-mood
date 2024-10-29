b548ce12bab0c3cfcbb51b8617c693579f8e7ea3
lolloscicchi <lorenzo.scicchitano@edu.itspiemonte.it>
2024-10-29 14:19:02 +0100

    yes

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // script.js
 
 // Timer di 24 ore
-let time = 24 * 60 * 60; // secondi in 24 ore
+var time = 1; // secondi in 24 ore
 
 function startTimer() {
   const timerElement = document.getElementById("timer");
   setInterval(() => {
-    const hours = Math.floor(time / 3600);
+    const hours = 0 + Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = time % 60;
     timerElement.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    time--;
+    time++;
   }, 1000);
 }
 
